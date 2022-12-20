ff859ae49f5c53c6bfe7329f1d1a7aa7ce17cb94
MaurineDuray <maurine.duray@proximus.be>
2022-12-20 09:28:18 +0100

    deuxième technique de router index.js

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -5,6 +5,7 @@ const Cars = () => {
         <>
             <h1>Liste des voitures</h1>
             <Outlet/>
+            {/* outlet va charger le composant car si j'ai une sous-route */}
         </>
      );
 }
--- src/components/Marque.js
+++ src/components/Marque.js
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 const Marque = () => {
     let {name} = useParams();
-    // crée une variable dans les hook qui s'appelle name
+    // recherche dans les param un paramètre qui s'appelle name et crée une variable dans les hook qui s'appelle name
     return <h1>Marque {name} </h1>;
 }
  
